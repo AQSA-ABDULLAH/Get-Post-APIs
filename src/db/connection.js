bb58a6ddb56e50a59fc7be6bd32e6e95d2969e95
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-07-18 14:00:15 +0500

    Establish database connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,10 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 
-const mongoose = require ("mongoose")
-
-mongoose.connect("mongodb://localhost:27017/students-records")
+mongoose.connect("mongodb://127.0.0.1/students-records", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connection is established.");
+}).catch((e) => {
+    console.log("No connection established",e);
+});
