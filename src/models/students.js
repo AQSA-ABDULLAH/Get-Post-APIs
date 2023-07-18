d9b90a08f8adceea4f2f86768a27cc2958c88878
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-07-18 14:01:05 +0500

    create models

---
+++ src/models/students.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: [true, "Email ID Already used."],
+        validator(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email")
+            }
+        }
+    },
+    phoneno:{
+        type: Number,
+        minlength: 11,
+        maxlength: 11,
+        required: true,
+        unique: true
+    },
+    address: {
+        type: String,
+        required: true,
+    }
+})
+
+//We create new collection here
+const Student = new mongoose.model('Student', studentSchema);
+
+module.exports = Student;
