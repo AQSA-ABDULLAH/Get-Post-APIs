f7dda44a73917b95c6ff8563b988d919b7d69be1
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-07-18 15:41:15 +0500

    Remove run time error

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
 const express = require("express");
+require("./src/db/connection")
+const Student = require("./src/models/students")
+
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.get("/", (req, res)=>{
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
     res.send("Hello from other side");
 
 })
 
-app.post("/student", (req, res)=>{
-    res.send("Hello from other side");
+// app.post("/student", (req, res) => {
+//     console.log(req.body);
+//     const user = new Student(req.body);
+//     user.save().then(() => {
+//         console.log(user);
+//         res.status(201).send(user);
+//     }).catch((e) => {
+//         res.status(400).send(e);
+//     })
+
+// })
+
+app.post("/student", async (req, res) => {
+
+    try {
+        const user = new Student(req.body);
+        const createUser = await user.save();
+        res.status(201).send(createUser);
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
 
 })
 
-app.listen(port, () => {
-    console.log(`connection is setup at port no ${port}`);
+//Read Data using ID
+app.get("/student", async (req, res) => {
+
+    try {
+
+        const studentData = await Student.find();
+        res.send(studentData)
+    }
+    catch(e){
+        res.status(400).send(e);
+    }
+
 })
